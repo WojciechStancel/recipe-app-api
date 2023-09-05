9c36a29d1005f7497797e6da58edd227bcae0cf1
Wojciech Stancel <wojciech.stancel@gmail.com>
2023-09-05 14:21:29 +0200

    setup django admin and users list

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, \
+    BaseUserManager, PermissionsMixin
 
 
 class UserManager(BaseUserManager):
@@ -21,7 +22,6 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-
 
 
 class User(AbstractBaseUser, PermissionsMixin):
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,7 +15,6 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-
     def test_new_user_email_normalized(self):
 
         sample_emails = [
@@ -33,7 +32,6 @@ class ModelTests(TestCase):
     def test_new_user_without_email_raises_error(self):
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
-
 
     def test_create_superuser(self):
         user = get_user_model().objects.create_superuser(
